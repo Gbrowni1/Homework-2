02a69393febc9143667298da11d543d0ee551c6f
GRANIT <gbrowni1@uwyo.edu>
2024-10-27 05:49:57 +0000

    Updated hw, not complete

--- Homework 2.py
+++ Homework 2.py
@@ -1,1 +1,58 @@
-print(12)
+# Grant Browning
+# UWYO COSC 1010
+# Submission Date: 10/26/24
+# HW 02
+# Lab Section: Austin
+# Sources, people worked with, help given to: GeeksforGeeks.org
+
+def Translator(text):
+    """Translates entered phrase to morse code."""
+
+    Alpha_Translate = {
+"A" : ".-",
+"B" : "-...",
+"C" : "-.-.",
+"D" : "-..",
+"E" : ".",
+"F" : "..-.",
+"G" : "--.",
+"H" : "....",
+"I" : "..",
+"J" : ".---",
+"K" : "-.-",
+"L" : ".-..",
+"M" : "--",
+"N" : "-.",
+"O" : "---",
+"P" : ".--.",
+"Q" : "--.-",
+"R" : ".-.",
+"S" : "...",
+"T" : "-",
+"U" : "..-",
+"V" : "...-",
+"W" : ".--",
+"X" : "-..-",
+"Y" : "-.--",
+"Z" : "--..",
+" " : " ", #Space
+}
+
+    morse_phrase = []
+
+#if string_variable.isalpha():
+
+    for letters in text.upper():
+        if letters in Alpha_Translate:
+            morse_phrase.append(Alpha_Translate[letters])
+
+        elif letters not in text.upper():
+            morse_phrase.append()
+
+
+    return ' '.join(morse_phrase)
+
+
+text = input("Please enter requested phrase to translate: ")
+morse_text = Translator(text)
+print(f"'{text}' translates to: \n {morse_text}")
